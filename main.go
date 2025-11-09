92810677d867f859aed339f69bfa185018c984e8
abhishekrgadad <abhishekgadad485@gmail.com>
2025-11-09 09:28:59 +0530

    [Add] project started with main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("server is running successfully")
+	})
+
+	log.Fatal(app.Listen(":8000"))
+
+}
